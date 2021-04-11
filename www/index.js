0e3108ef3c53a1d0a25f83b90ec6c12dbbe96383
Nicolas Frankel <nicolas@frankel.ch>
2021-04-11 16:10:51 +0200

    Render with JavaScript
      https://rustwasm.github.io/docs/book/game-of-life/implementing.html#rendering-with-javascript

--- www/index.js
+++ www/index.js
@@ -1,3 +1,13 @@
-import * as wasm from "hello-wasm-pack";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet();
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+
+    requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
