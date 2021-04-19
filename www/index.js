10c49aaf548f8a4b5f4a6a18ac2bff3cd26c07b4
Nicolas Frankel <nicolas@frankel.ch>
2021-04-19 22:22:22 +0200

    Creating a Frames Per Second Timer
      https://rustwasm.github.io/docs/book/game-of-life/time-profiling.html#creating-a-frames-per-second-timer-with-the-windowperformancenow-function

--- www/index.js
+++ www/index.js
@@ -21,7 +21,8 @@ const ctx = canvas.getContext('2d');
 let animationId = null;
 
 const renderLoop = () => {
-    debugger;
+    fps.render(); //new
+
     universe.tick();
 
     drawGrid();
@@ -171,6 +172,49 @@ const pulsar = document.createElement("option");
 pulsar.value = "pulsar";
 pulsar.text = "🌟";
 onClickSelect.add(pulsar);
+
+const fps = new class {
+    constructor() {
+        this.fps = document.getElementById("fps");
+        this.frames = [];
+        this.lastFrameTimeStamp = performance.now();
+    }
+
+    render() {
+        // Convert the delta time since the last frame render into a measure
+        // of frames per second.
+        const now = performance.now();
+        const delta = now - this.lastFrameTimeStamp;
+        this.lastFrameTimeStamp = now;
+        const fps = 1 / delta * 1000;
+
+        // Save only the latest 100 timings.
+        this.frames.push(fps);
+        if (this.frames.length > 100) {
+            this.frames.shift();
+        }
+
+        // Find the max, min, and mean of our 100 latest timings.
+        let min = Infinity;
+        let max = -Infinity;
+        let sum = 0;
+        for (let i = 0; i < this.frames.length; i++) {
+            sum += this.frames[i];
+            min = Math.min(this.frames[i], min);
+            max = Math.max(this.frames[i], max);
+        }
+        let mean = sum / this.frames.length;
+
+        // Render the statistics.
+        this.fps.textContent = `
+Frames per Second:
+         latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+`.trim();
+    }
+};
 
 drawGrid();
 drawCells();
