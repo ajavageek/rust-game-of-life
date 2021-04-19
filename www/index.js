64838a2144eaeb5e0245b4a673864bfef7e521a9
Nicolas Frankel <nicolas@frankel.ch>
2021-04-19 22:55:26 +0200

    Growing our Game of Life Universe
      https://rustwasm.github.io/docs/book/game-of-life/time-profiling.html#growing-our-game-of-life-universe

--- www/index.js
+++ www/index.js
@@ -83,13 +83,32 @@ const drawCells = () => {
 
     ctx.beginPath();
 
+    // Alive cells.
+    ctx.fillStyle = ALIVE_COLOR;
     for (let row = 0; row < height; row++) {
         for (let col = 0; col < width; col++) {
             const idx = getIndex(row, col);
+            if (!bitIsSet(idx, cells)) {
+                continue;
+            }
 
-            ctx.fillStyle = bitIsSet(idx, cells)
-                ? ALIVE_COLOR
-                : DEAD_COLOR;
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+
+    // Dead cells.
+    ctx.fillStyle = DEAD_COLOR;
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col);
+            if (bitIsSet(idx, cells)) {
+                continue;
+            }
 
             ctx.fillRect(
                 col * (CELL_SIZE + 1) + 1,
